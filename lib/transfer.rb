f47875fed7e7de04a473c2be5e77843c9dee08f9
Erica Little <eteney90@gmail.com>
2021-04-11 15:19:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
 
-  attr_accessor :sender, :receiver, :status
+  attr_accessor :sender, :receiver, :status, :transfer_amount
 
-  def initialize(sender, receiver, status = "pending")
+  def initialize(sender, receiver, status = "pending", transfer_amount)
     @sender = sender
-    @receiver = receiver 
-    @status = status 
+    @receiver = receiver
+    @status = status
+    @transfer_amount = transfer_amount
   end
 
 end
