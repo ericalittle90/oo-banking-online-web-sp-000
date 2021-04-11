0da1a52fe03b85f12055871d441bcdbe4dd704c3
Erica Little <eteney90@gmail.com>
2021-04-11 15:05:22 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :bank_account
+
+  def initialize(sender, receiver) 
+    @sender = sender
+    @receiver = receiver 
+  end 
+  
 end
