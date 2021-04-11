3e76f22f9d007e6c3ac4225aa3cce31e854ea4a2
Erica Little <eteney90@gmail.com>
2021-04-11 14:58:58 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -3,7 +3,7 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :transfer, :name
 
-  def initialize(name)
+  def initialize(name, status)
     @name = name
     @balance = 1000
     @status = open
